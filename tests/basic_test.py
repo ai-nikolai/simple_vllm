9c7846ebdf9814b36fee6d1a250597d843ab67a0
Nikolai Rozanov <nikolai.rozanov@gmail.com>
2025-05-28 21:16:00 +0100

    added testing from README

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,28 +1,15 @@
-from simple import get_vllm_generator
+from simple_vllm import get_vllm_generator
 
-# Create the client.
-client = get_vllm_generator(
-    model_name="Qwen/Qwen2.5-3B-Instruct",
-    max_seq_length = 4096,
-    gpu_memory_utilization=0.5,
-    load_in_4bit = True,
-)
+if __name__== "__main__":
+    # Create the client.
+    client = get_vllm_generator(model_name="Qwen/Qwen2.5-3B-Instruct")
 
-# Create a messages object in OpenAI format.
-messages = [
-    {
-        "role" : "user",
-        "content" : "Hello, could you tell me how to become a better human?",
-    },
-]
+    # Get the response as list of str
+    responses = client.generate("Hello, could you tell me how to become a better human")
 
-# Get the response as list of str
-responses = client.generate(
-    messages, 
-    num_generations=1,
-    max_completion_tokens=100,
-    temperature=0.0,
-    )
+    print(responses[0])
 
-# print response.
-print(responses[0]) #this will print the model output.
+# from simple_vllm import get_vllm_generator
+# client = get_vllm_generator(model_name="Qwen/Qwen2.5-3B-Instruct")
+# responses = client.generate("Hello, could you tell me how to become a better human")
+# print(responses[0])
